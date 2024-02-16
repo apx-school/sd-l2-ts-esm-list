b2fa1c36489623201e65214ea52af9ff5004936b
Marcelo Zapaia <marcelo.zapaia@bluealba.com>
2024-02-16 08:37:10 -0300

    initial commit

---
+++ src/products.ts
@@ -0,0 +1,19 @@
+import productsList from "./products.json" assert { type: "json" };
+
+export function productsListComponent() {
+  const section = document.createElement("section");
+  section.style.border = "solid 1px #ccc";
+  section.style.padding = "10px";
+
+  const list = document.createElement("ul");
+
+  productsList.forEach((product) => {
+    const listItem = document.createElement("li");
+    listItem.textContent = `${product.title} - $${product.price}`;
+    list.appendChild(listItem);
+  });
+
+  section.appendChild(list);
+
+  return section;
+}
