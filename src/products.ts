ae3b6dc0306ad6dc249251b0529e87ec62a44520
Marcelo Zapaia <zapaiamarce@gmail.com>
2024-07-11 20:48:39 -0300

    wix assert

--- src/products.ts
+++ src/products.ts
@@ -1,4 +1,6 @@
-import productsList from "./products.json" assert { type: "json" };
+// assert { type: "json" } ya no va
+// https://stackoverflow.com/questions/70106880/err-import-assertion-type-missing-for-import-of-json-file
+import productsList from "./products.json" with { type: "json" };
 
 export function productsListComponent() {
   const section = document.createElement("section");
